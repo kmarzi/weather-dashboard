107dfb6b1e9d247de5051cd83b6f8d017076a15d
Kmarzi <k.l.marziali@gmail.com>
2020-08-13 21:28:12 -0400

    added variables to js

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+//create variables
+var api = "f22c7df987b77fbf3f4c8bf5eb71a13f"
+var userSearch = document.getElementById("search-button")
+var userInput = document.getElementById("city-input")
+var clear = document.getElementById("clear-history")
+var currentTemp = document.getElementById("temperature")
+var currentWind = document.getElementById("wind-speed")
+var currentHumidity = document.getElementById("humidity")
+var currentUV = document.getElementById("UV-index")
+var history = document.getElementById("history")
+
+
+
+
