d3e46e6ff70976c0fa5149123657fd3a7e0f5524
Kmarzi <k.l.marziali@gmail.com>
2020-08-14 19:44:53 -0400

    created a variable for unique cities and created a for loop

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,36 @@ searchClicker();
 init(); 
 listClicker(); 
 
+
+//function for saved citiesgit 
+function init(){
+    let saved_cities = JSON.parse(localStorage.getItem("cities"));
+
+    if (saved_cities !== null){
+        cities = saved_cities
+    }   
+    
+    renderButtons(); 
+}
+
+function storeCities(){
+    localStorage.setItem("cities", JSON.stringify(cities)); 
+}
+//render buttons for each element in cities array as a search history for user
+function renderButtons(){
+    citiesDiv.innerHTML = ""; 
+    if(cities == null){
+        return;
+    }
+    var unique_cities = [...new Set(cities)];
+    for(var i=0; i < unique_cities.length; i++){
+        var cityName = unique_cities[i]; 
+
+        var buttonEl = document.createElement("button");
+        buttonEl.textContent = cityName; 
+        buttonEl.setAttribute("class", "listbtn"); 
+
+        citiesDiv.appendChild(buttonEl);
+        listClicker();
+      }
+    }
