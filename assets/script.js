5caa99b66e3194a1c846fb57b54119c350224f11
Kmarzi <k.l.marziali@gmail.com>
2020-08-14 19:11:49 -0400

    added variables and set empty arrays

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// make variables// set empty arrays//
+var city=""; 
+var url="";
+var APIkey="";
+var queryurl ="";
+var currenturl = "";
+var citiesDiv = document.getElementById("searched_cities_container");
+
+//empty arrays
+cities = []; 
+searchClicker(); 
+init(); 
+listClicker(); 
+
